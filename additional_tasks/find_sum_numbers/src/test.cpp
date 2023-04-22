ab58f4a3e802ded49c8aeec4004e52f88974cabf
Ivan Khokhlov <lostpointmeister@gmail.com>
2023-04-22 23:52:43 +0300

    new additional task and remove some clang-tidy warnings (#48)

---
+++ additional_tasks/find_sum_numbers/src/test.cpp
@@ -0,0 +1,8 @@
+
+#include <gtest/gtest.h>
+
+#include <stack>
+
+#include "utils.hpp"
+
+TEST(GetNumbersOfSum, Simple) {}
---
+++ additional_tasks/find_sum_numbers/src/utils.cpp
@@ -0,0 +1,7 @@
+#include "utils.hpp"
+
+#include <algorithm>
+
+std::pair<int, int> GetNumbersOfSum(const std::vector<int>& numbers, int sum) {
+  return {0, 0};
+}
